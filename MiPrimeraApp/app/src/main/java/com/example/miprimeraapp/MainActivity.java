0a9eeb5e327ee7a2741a6684b9fcb453a2f97925
rosemary <jasonadonayv@gmail.com>
2026-02-18 16:46:46 -0600

    Finalizacion de la tarea de conversores

--- MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
