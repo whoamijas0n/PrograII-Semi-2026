8f030d6767bce8e8f983239e4a7862570769c77c
rosemary <jasonadonayv@gmail.com>
2026-03-04 14:39:25 -0600

    Rama de sensor de acelerometro completa

--- MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorProximidad();
+        sensorAcelerometro();
     }
     private void iniciar(){
         sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
@@ -48,24 +48,21 @@ public class MainActivity extends AppCompatActivity {
     private void detener(){
         sensorManager.unregisterListener(sensorEventListener);
     }
-    private void sensorProximidad(){
-        tempVal = findViewById(R.id.lblSensorProximidad);
+    private void sensorAcelerometro(){
+        tempVal = findViewById(R.id.lblSensorAcelerometro);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if(sensor==null){
-            tempVal.setText("No dispones del sensor de proximidad");
+            tempVal.setText("No dispones del sensor de acelerometro");
             finish();
         }
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                double valor = sensorEvent.values[0];
-                tempVal.setText("Prxomidad: "+ valor);
-                int color = Color.BLACK;
-                if(valor<=4){
-                    color = Color.WHITE;
-                }
-                getWindow().getDecorView().setBackgroundColor(color);
+                double x = sensorEvent.values[0];
+                double y = sensorEvent.values[1];
+                double z = sensorEvent.values[2];
+                tempVal.setText("Desplazamiento: X: "+ x +"\n; Y: "+y +"\n; Z: "+z);
             }
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
