70da9a2d9e6a5e78fb1b03534656ec3e5fcf78bd
rousemary <rousemary@DESKTOP-5AD1KOK>
2026-04-22 17:52:01 -0600

    Agregue toda la rama de agregar amigos con sql

--- MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -1,92 +1,154 @@
 package com.example.miprimeraapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends Activity {
+    DB db;
     Button btn;
-    Spinner spn;
+    TextView tempVal;
+    String accion="nuevo", idAmigo="", urlFoto;
+    Intent tomarFotoIntent;
+    FloatingActionButton fab;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v -> calcular());
+        img = findViewById(R.id.imgFotoAmigo);
+        img.setOnClickListener(v->tomarFoto());
+
+        db = new DB(this);
+
+        btn = findViewById(R.id.btnGuardarAmigo);
+        btn.setOnClickListener(v->guardarAmigo());
+
+        fab = findViewById(R.id.fabListaAmigo);
+        fab.setOnClickListener(v->regresarListaAmigos());
+
+        mostrarDatosAmigos();
+    }
+    private void mostrarDatosAmigos(){
+        try{
+            Bundle parametros = getIntent().getExtras();
+            accion = parametros.getString("accion");
+            if(accion.equals("modificar")){
+                JSONObject datos = new JSONObject(parametros.getString("amigos"));
+                idAmigo = datos.getString("idAmigo");
+
+                tempVal = findViewById(R.id.txtNombreAmigos);
+                tempVal.setText(datos.getString("nombre"));
+
+                tempVal = findViewById(R.id.txtDireccionAmigos);
+                tempVal.setText(datos.getString("direccion"));
+
+                tempVal = findViewById(R.id.txtTelefonoAmigos);
+                tempVal.setText(datos.getString("telefono"));
+
+                tempVal = findViewById(R.id.txtEmailAmigos);
+                tempVal.setText(datos.getString("email"));
+
+                tempVal = findViewById(R.id.txtDuiAmigos);
+                tempVal.setText(datos.getString("dui"));
+
+                urlFoto = datos.getString("foto");
+                img.setImageURI(Uri.parse(urlFoto));
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al mostrar los datos: "+ e.getMessage());
+        }
+    }
+    private void tomarFoto(){
+        tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File fotoAmigo = null;
+
+        try{
+            fotoAmigo = crearImgAmigo();
+            if(fotoAmigo!=null){
+                Uri uriFoto = FileProvider.getUriForFile(MainActivity.this, "com.ugb.miprimeraapp.fileprovider", fotoAmigo);
+                tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFoto);
+                startActivityForResult(tomarFotoIntent, 1);
+            }else{
+                mostrarMsg("Nose pudo crear la foto");
+            }
+        } catch (Exception e) {
+            mostrarMsg("Error al tomar la foto: "+ e.getMessage());
+        }
     }
 
-    private void calcular() {
-        try {
-            // Leer num1 (Si está vacío, lo tomamos como 0.0 para evitar errores)
-            tempVal = findViewById(R.id.txtNum1);
-            String strNum1 = tempVal.getText().toString();
-            double num1 = strNum1.isEmpty() ? 0.0 : Double.parseDouble(strNum1);
-
-            // Leer num2 (Si está vacío, lo tomamos como 0.0)
-            tempVal = findViewById(R.id.txtNum2);
-            String strNum2 = tempVal.getText().toString();
-            double num2 = strNum2.isEmpty() ? 0.0 : Double.parseDouble(strNum2);
-
-            double respuesta = 0;
-
-            spn = findViewById(R.id.cboOpciones);
-            switch (spn.getSelectedItemPosition()) {
-                case 0: // Suma
-                    respuesta = num1 + num2;
-                    break;
-                case 1: // Resta
-                    respuesta = num1 - num2;
-                    break;
-                case 2: // Multiplicacion
-                    respuesta = num1 * num2;
-                    break;
-                case 3: // Division
-                    if (num2 != 0) {
-                        respuesta = num1 / num2;
-                    } else {
-                        Toast.makeText(this, "No se puede dividir entre 0", Toast.LENGTH_SHORT).show();
-                        respuesta = 0;
-                    }
-                    break;
-                case 4: // Factorial (Calculado en base a num1)
-                    if (num1 < 0) {
-                        Toast.makeText(this, "No hay factorial de negativos", Toast.LENGTH_SHORT).show();
-                    } else {
-                        respuesta = 1;
-                        for (int i = 1; i <= (int) num1; i++) {
-                            respuesta *= i;
-                        }
-                    }
-                    break;
-                case 5: // Porcentaje (El num1 % de num2)
-                    respuesta = (num1 * num2) / 100.0;
-                    break;
-                case 6: // Exponenciacion (num1 elevado a num2)
-                    respuesta = Math.pow(num1, num2);
-                    break;
-                case 7: // Raiz (num1 es la base, num2 es el indice de la raiz)
-                    if (num2 == 0) {
-                        Toast.makeText(this, "El índice de la raíz no puede ser 0", Toast.LENGTH_SHORT).show();
-                    } else {
-                        respuesta = Math.pow(num1, 1.0 / num2);
-                    }
-                    break;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if(requestCode==1 && resultCode==RESULT_OK){
+                img.setImageURI(Uri.parse(urlFoto));
+            }else{
+                mostrarMsg("No fue posible mostrar la foto");
             }
-
-            // Mostrar el resultado
-            tempVal = findViewById(R.id.lblRespuesta);
-            tempVal.setText("Respuesta: " + respuesta);
-
         } catch (Exception e) {
-            // Si algo sale mal (ej. texto no válido), mostramos un mensaje en lugar de que la app se cierre
-            Toast.makeText(this, "Por favor, ingresa números válidos", Toast.LENGTH_SHORT).show();
+            mostrarMsg("Error en abrir la camara: "+ e.getMessage());
         }
+    }
+
+    private File crearImgAmigo() throws Exception{
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
+                fileMane = "foto_"+ fechaHoraMs;
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if(dirAlmacenamiento.exists()==false){
+            dirAlmacenamiento.mkdir();
+        }
+        File image = File.createTempFile(fileMane, ".jpg", dirAlmacenamiento);
+        urlFoto = image.getAbsolutePath();
+        return image;
+    }
+    private void guardarAmigo(){
+        tempVal = findViewById(R.id.txtNombreAmigos);
+        String nombre = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtDireccionAmigos);
+        String direccion = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtTelefonoAmigos);
+        String tel = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtEmailAmigos);
+        String email = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtDuiAmigos);
+        String dui = tempVal.getText().toString();
+
+        String[] datos = {idAmigo, nombre, direccion, tel, email, dui, urlFoto};
+        db.administrar_amigos(accion, datos);
+        mostrarMsg("Registro de amigo guardado con exito.");
+
+        regresarListaAmigos();
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+    private void regresarListaAmigos(){
+        Intent intent = new Intent(this, lista_amigos.class);
+        startActivity(intent);
     }
 }
